40ce68ed446d689e2be781916a9f34e5ec8bfdc4
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-04-05 13:33:24 +0900

    Middlewares

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
 function fetcher(url) {
@@ -5,7 +6,11 @@ function fetcher(url) {
 }
 
 export default function Index() {
-  const { data, error } = useSWR('/api/randomQuote', fetcher);
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
   // The following line has optional chaining, added Next.js v9.1.5
   // is the same as `data && data.author`
   const author = data?.author;
